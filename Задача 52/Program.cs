bd8bc46b467ed140b06c3c3e8d9f5623839291fd
Liliya <liliya.demchik.13@mail.ru>
2022-11-10 21:02:32 +0500

    7

---
+++ Задача 52/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Number(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            if (i < array.GetLength(0)) sum += array[i, j];
+        }
+        Console.WriteLine($"Среднее арифметическое = {sum / array.GetLength(0)} ");
+    }
+    return;
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] numArray = GetArray(row, columns, 0, 10);
+PrintArray(numArray);
+Number(numArray);
+
---
+++ Задача со звездочкой/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача со звездочкой: Написать программу для перевода римских чисел в десятичные арабские.
+// III -> 3
+// LVIII -> 58
+// MCMXCIV -> 1994
+
+
+
+void Rome(string num)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (num[i] == 'I') array[i] = 1;
+        if (num[i] == 'V') array[i] = 5;
+        if (num[i] == 'X') array[i] = 10;
+        if (num[i] == 'L') array[i] = 50;
+        if (num[i] == 'C') array[i] = 100;
+        if (num[i] == 'D') array[i] = 500;
+        if (num[i] == 'M') array[i] = 1000;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите римское число: ");
+string rim = int.Parse(Console.ReadLine()!);
+Rome(rim);
